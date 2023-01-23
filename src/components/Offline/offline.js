237559cde6578f652e65a6d223fbc7571b60e011
Jordan Schultz <jmschultz@ucdavis.edu>
2023-01-23 09:45:44 -0800

    added styling to offline dropdown

--- src/components/Offline/offline.js
+++ src/components/Offline/offline.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { Drawer, Col, Row } from 'antd';
 import wifi from '../../assets/img/wifi-connection-offline-icon.png'
+import './offline.css'
 
 export default function Offline({ children }) {
     const [online, setOnline] = useState(window.navigator.onLine)
@@ -39,14 +40,12 @@ export default function Offline({ children }) {
                 onClose={onClose}
                 open={open}
             >
-                <Row align="middle">
-                    <Col span={4}>
-                        <img width={40} src={wifi}></img>
-                    </Col>
-                    <Col span={20}>
+                <Row align="middle" justify='center'>
+                    <img style={{display: 'inline', marginRight: '35px'}} width={40} src={wifi}></img>
+                    <div style={{display: 'inline'}}>
                         <h3>You are offline!</h3>
                         <p>Please check your internet connection.</p>
-                    </Col>
+                    </div>
                 </Row>
             </Drawer>
             {children}
